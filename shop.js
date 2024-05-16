acac0a1742953febbb0c7080c3a19313afaba80e
prasadp1307 <prasaddp1137@gmail.com>
2024-05-16 19:04:20 +0530

    views(HTML-CSS-JS)

---
+++ shop.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+const express = require('express');
+
+const router = express.Router();
+const rootDir = require('./util/path')
+
+router.get('/',(req,res,next) => {
+    res.sendFile(path.join(rootDir,'views','shop.html'))
+});
+
+router.get('/admin/contactus',(req,res,next) => {
+    res.sendFile(path.join(rootDir,'views','index.html'))
+});
+module.exports = router;
